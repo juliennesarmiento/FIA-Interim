57d3215ffc1ed7349ce7f31cad34a4d1cfed3fda
Julienne <1505124F@student.tp.edu.sg>
2017-08-14 17:16:23 +0800

    Add files via upload

---
+++ bash 3.7.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#######################
+
+reset
+
+if [ "$EUID" -ne 0 ] ; then
+	printf "\e[31mPlease run as root!\n"
+	printf "Press any key to exit\e[0m\n"
+	read -n 1 -s
+	exit
+fi
+
+if [ -e /etc/redhat-release ] ; then
+	printf "\e[1mRunning Scan for "
+	printf "$(cat /etc/redhat-release)"
+	printf "\e[0m\n"
+else
+	printf "\e[31m\e[1mYou are not on a Red Hat System!\n"
+	printf "Press any key to exit\e[0m\n"
+	read -n 1 -s
+	kill -9 $PPID
+fi
+
+function ctrl_C() {
+	kill -9 $PPID	
+}
+
+function ctrl_Z() {
+	kill -9 $PPID
+}
+
+trap ctrl_C INT
+trap ctrl_Z 2 20
+
+######################
+#3.7 Remove LDAP
+printf "\n\e[1mAttempting to remove Lightweight Directory Access Protocol(LDAP)if existing.\e[0m\n"
+yum -y erase openldap-clients
+yum -y erase openldap-servers
+printf "\e[32mLDAP not installed.\e[0m\n"
+
+printf "\e[32mCompleted!\n"
+printf "Press any key to exit\e[0m\n"
+
+#This kills the process, please remove if not needed.
+read -n 1 -s
+kill -9 $PPID
