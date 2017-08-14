e8f83449e639d8f80721a9d0828038b081d1739b
1502597f <daphnetrs@gmail.com>
2017-08-14 17:25:36 +0800

    Update bash 2.6.sh

--- bash 2.6.sh
+++ bash 2.6.sh
@@ -35,6 +35,7 @@ trap ctrl_Z 2 20
 
 ######################
 #2.6
+
 printf "\e[1mDisable chargen-dgram\e[0m\n"
 if yum list xinetd | grep "Available Packages" >/dev/null ; then
 	printf "\e[32mAlready removed Chargen-Dgram from removing Xinetd.\e[0m\n"
@@ -49,5 +50,6 @@ printf "\e[32mCompleted!\n"
 printf "Press any key to exit\e[0m\n"
 
 #This kills the process, please remove if not needed.
+
 read -n 1 -s
 kill -9 $PPID
