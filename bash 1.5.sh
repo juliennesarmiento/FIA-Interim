47744f003f7e5218a57b1303856bfbddbaa05c62
winnateng <1501710F@student.tp.edu.sg>
2017-08-14 17:26:16 +0800

    Update bash 1.5.sh

--- bash 1.5.sh
+++ bash 1.5.sh
@@ -36,6 +36,7 @@ trap ctrl_Z 2 20
 ######################
 
 #1.5 /var
+
 printf "\e[1mCreating separate /var partition.\e[0m\n"
 if lvcreate -l 10%VG -n var rhel ; then
 	echo "/dev/rhel/var	/var	ext4	defaults	0 0" >> /etc/fstab
@@ -48,5 +49,6 @@ printf "\e[32mCompleted!\n"
 printf "Press any key to exit\e[0m\n"
 
 #This kills the process, please remove if not needed.
+
 read -n 1 -s
 kill -9 $PPID
