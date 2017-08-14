2d238829e1ddcf407aaea71b58728e7d64be94e7
winnateng <1501710F@student.tp.edu.sg>
2017-08-14 17:17:58 +0800

    Add files via upload

---
+++ bash 1.9 to 1.10.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#######################
+
+reset
+
+if [ "$EUID" -ne 0 ] ; then
+	printf "\e[31mPlease run as root!\n"
+	printf "Press any key to exit\e[0m\n"
+	read -n 1 -s
+	exit
+fi
+
+if [ -e /etc/redhat-release ] ; then
+	printf "\e[1mRunning Scan for "
+	printf "$(cat /etc/redhat-release)"
+	printf "\e[0m\n"
+else
+	printf "\e[31m\e[1mYou are not on a Red Hat System!\n"
+	printf "Press any key to exit\e[0m\n"
+	read -n 1 -s
+	kill -9 $PPID
+fi
+
+function ctrl_C() {
+	kill -9 $PPID	
+}
+
+function ctrl_Z() {
+	kill -9 $PPID
+}
+
+trap ctrl_C INT
+trap ctrl_Z 2 20
+
+######################
+#1.9-1.10 /home
+printf "\e[1mCreating separate partition for /home and setting options.\e[0m\n"
+if lvcreate -l 10%VG -n home rhel ; then
+	echo "/dev/rhel/home	/home	ext4	nodev	0 0" >> /etc/fstab
+	printf "\e[32mExecuted.\e[0m\n"
+else
+	printf "\e[31mUnable to create /home!\e[0m\n"
+fi
+
+printf "\e[32mCompleted!\n"
+printf "Press any key to exit\e[0m\n"
+
+#This kills the process, please remove if not needed.
+read -n 1 -s
+kill -9 $PPID
