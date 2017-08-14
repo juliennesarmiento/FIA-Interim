84172065fdcdfae377fe6c98dd669134e427dbd5
winnateng <1501710F@student.tp.edu.sg>
2017-08-14 17:16:11 +0800

    Add files via upload

---
+++ bash 1.6.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#######################
+
+reset
+
+if [ "$EUID" -ne 0 ] ; then
+	printf "\e[31mPlease run as root!\n"
+	printf "Press any key to exit\e[0m\n"
+	read -n 1 -s
+	exit
+fi
+
+if [ -e /etc/redhat-release ] ; then
+	printf "\e[1mRunning Scan for "
+	printf "$(cat /etc/redhat-release)"
+	printf "\e[0m\n"
+else
+	printf "\e[31m\e[1mYou are not on a Red Hat System!\n"
+	printf "Press any key to exit\e[0m\n"
+	read -n 1 -s
+	kill -9 $PPID
+fi
+
+function ctrl_C() {
+	kill -9 $PPID	
+}
+
+function ctrl_Z() {
+	kill -9 $PPID
+}
+
+trap ctrl_C INT
+trap ctrl_Z 2 20
+
+######################
+
+#1.6 bind mount
+printf "\e[1mBinding mount.\e[0m\n"
+printf "/tmp /var/tmp none bind 0 0" >> /etc/fstab
+mount --bind /tmp /var/tmp
+printf "\e[32mExecuted.\e[0m\n"
+
+printf "\e[32mCompleted!\n"
+printf "Press any key to exit\e[0m\n"
+
+#This kills the process, please remove if not needed.
+read -n 1 -s
+kill -9 $PPID
