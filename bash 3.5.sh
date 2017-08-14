1f82d233859774891ed40663d343d03946828310
Julienne <1505124F@student.tp.edu.sg>
2017-08-14 17:25:35 +0800

    Update bash 3.5.sh

--- bash 3.5.sh
+++ bash 3.5.sh
@@ -35,6 +35,7 @@ trap ctrl_Z 2 20
 
 ######################
 #3.5 Remove DHCP Server
+
 printf "\n\e[1mAttempting to disable DHCPD if exists.\e[0m\n"
 if systemctl is-active dhcpd | grep "active" >/dev/null && systemctl is-enabled dhcpd | grep "enabled" >/dev/null ; then
 	printf "\e[31mDHCPD is not disabled. Attempting to disable now.\e[0m\n"
@@ -54,5 +55,6 @@ printf "\n\e[34mAttempting to remove any existing dhcp files.\e[0m\n"
 printf "Press any key to exit\e[0m\n"
 
 #This kills the process, please remove if not needed.
+
 read -n 1 -s
 kill -9 $PPID
