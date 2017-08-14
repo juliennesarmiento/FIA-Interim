17c185e8f82ec3ee4706d20bf39d4197ffa0f7a4
Julienne <1505124F@student.tp.edu.sg>
2017-08-14 17:26:14 +0800

    Update bash 3.4.sh

--- bash 3.4.sh
+++ bash 3.4.sh
@@ -35,6 +35,7 @@ trap ctrl_Z 2 20
 
 ######################
 #3.4 Disable CUPS Print Server
+
 printf "\n\e[1mAttempting to disable CUPS Print Server.\e[0m\n"
 
 if systemctl is-active cups | grep "active" >/dev/null && systemctl is-enabled cups | grep "enabled" >/dev/null ; then
@@ -53,5 +54,6 @@ printf "\e[32mCompleted!\n"
 printf "Press any key to exit\e[0m\n"
 
 #This kills the process, please remove if not needed.
+
 read -n 1 -s
 kill -9 $PPID
